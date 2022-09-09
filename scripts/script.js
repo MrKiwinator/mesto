1d12f17de6c48f268ca9133b79b3bdfabf131883
Mikhail Legostaev <legostaev.michel@gmail.com>
2022-09-09 14:01:20 +0300

    made corrections as per reviewer remarks

--- scripts/script.js
+++ scripts/script.js
@@ -2,53 +2,46 @@
 
 // Profile variables:
 
-let profile = document.querySelector(".profile");
+const profile = document.querySelector(".profile");
 
-let profileName = profile.querySelector(".profile__name");
-let profileStatus = profile.querySelector(".profile__status");
+const profileName = profile.querySelector(".profile__name");
+const profileStatus = profile.querySelector(".profile__status");
 
-let editBtn = profile.querySelector(".profile__edit-button");
-let addBtn = profile.querySelector(".profile__add-button");
+const editBtn = profile.querySelector(".profile__edit-button");
+const addBtn = profile.querySelector(".profile__add-button");
 
 // Popup form variables:
 
-let popup = document.querySelector(".popup");
-let formElement = popup.querySelector(".popup__form");
-let userNameInput = popup.querySelector("#username");
-let userStatusInput = popup.querySelector("#user-info");
-let submitBtn = popup.querySelector(".popup__submit");
-let popupCloseBtn = popup.querySelector(".popup__close-btn");
+const popup = document.querySelector(".popup");
+const formElement = popup.querySelector(".popup__form");
+const userNameInput = popup.querySelector(".popup__username");
+const userStatusInput = popup.querySelector(".popup__userstatus");
+const popupCloseBtn = popup.querySelector(".popup__close-btn");
 
-// Popup state change:
+// Popup window:
 
-function popupStageChange() {
-    popup.classList.toggle("popup_opened");
+function closingPopup() {
+    popup.classList.remove("popup_opened");
 }
 
-editBtn.addEventListener("click", popupStageChange);
+function openingPopup() {
+    popup.classList.add("popup_opened");
 
-popupCloseBtn.addEventListener("click", popupStageChange);
+    userNameInput.value = profileName.textContent;
+    userStatusInput.value = profileStatus.textContent;
+}
 
-// Username and Status:
-
-userNameInput.setAttribute("value", profileName.textContent);
-userStatusInput.setAttribute("value", profileStatus.textContent);
-
-function formSubmitHandler (evt) {
+function submitPopup(evt) {
     evt.preventDefault();
 
-    // Username change:
-    let userNameInputText = userNameInput.value;
+    profileName.textContent = userNameInput.value;
+    profileStatus.textContent = userStatusInput.value;
 
-    profileName.textContent = userNameInputText;
-
-    // User status change:
-    let userStatusInputText = userStatusInput.value;
-
-    profileStatus.textContent = userStatusInputText;
-
-    // Popup closing:
-    popupStageChange();
+    closingPopup();
 }
 
-formElement.addEventListener("submit", formSubmitHandler);
+editBtn.addEventListener("click", openingPopup);
+
+popupCloseBtn.addEventListener("click", closingPopup);
+
+formElement.addEventListener("submit", submitPopup);
